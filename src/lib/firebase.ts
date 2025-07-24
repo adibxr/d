b1e1f72128d94337a51d8b3173c731ecd312ff51
Aditya Raj <ccidcop@gmail.com>
2025-07-24 13:47:51 +0000

    for database connect to this app

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,14 @@ import { getDatabase, ref, get, set, push, remove, child } from 'firebase/databa
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCD5FoayYfZlMeNHph5QCBxVR7FF851R6w",
-  authDomain: "article-8a9a1.firebaseapp.com",
-  databaseURL: "https://website-473ae-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  projectId: "article-8a9a1",
-  storageBucket: "article-8a9a1.firebasestorage.app",
-  messagingSenderId: "537477655160",
-  appId: "1:537477655160:web:6cec627306d71c4cea1b8d"
+  apiKey: "AIzaSyALo9qIYAH0oe5eYBzBSpC_a_GUSanfHMM",
+  authDomain: "website-473ae.firebaseapp.com",
+  databaseURL: "https://website-473ae-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "website-473ae",
+  storageBucket: "website-473ae.firebasestorage.app",
+  messagingSenderId: "727463953378",
+  appId: "1:727463953378:web:9864fa5d1611158a9d2fbb",
+  measurementId: "G-ZM6E3BECSD"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
