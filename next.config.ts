81d1be069a3d0496dcfb5e3f33467914e1e06cf6
Aditya Raj <ccidcop@gmail.com>
2025-07-30 11:48:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
           hostname: 'www.cloudflare.com',
           port: '',
           pathname: '/**'
+      },
+      {
+          protocol: 'https',
+          hostname: 'entrepreneurhandbook.co.uk',
+          port: '',
+          pathname: '/**'
       }
     ],
   },
